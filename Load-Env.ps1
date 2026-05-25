26ecdf92c233e97c7938018ea25aa62645b559b6
Studio Butter <bebosua05@yahoo.com.vn>
2026-05-25 13:05:22 +0700

    Envioriment Loader

---
+++ Load-Env.ps1
@@ -0,0 +1,8 @@
+# Load .env file and set environment variables
+Get-Content .env | ForEach-Object {
+    if ($_ -match "^\s*([^#][^=]+)=(.*)$") {
+        $name = $matches[1].Trim()
+        $value = $matches[2].Trim()
+        [System.Environment]::SetEnvironmentVariable($name, $value, "Process")
+    }
+}
